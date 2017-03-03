09ef01b7d960cba8d2a7049f52c36b635964f697
An Duong <an@nimbl3.com>
2017-03-03 15:09:47 +0700

    Add notificaton to answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,4 @@
 class AnswersController < ApplicationController
-  API_KEY = 'AAAAyXAL1fY:APA91bFQl0V1o3qR5Fk7W_jnJBDkMcHkW_2DJ0cxXVKpeDGtqQ1BO_4olRzowpUNEcmxh4hyOlRUgluN-XPpZumH5_MPLajBz2FHWGmuA2LWVLeZGPNYvVA20lcmBfdTOd9rZ14wBcmp'
-
   def create
     answer = Answer.new(permitted_attributes)
     answer.save!
@@ -14,22 +12,5 @@ class AnswersController < ApplicationController
     params
       .require(:answer)
       .permit(:content, :question_id)
-  end
-
-  def webpush
-    subscription = Subscription.first
-    message = {
-      title: 'New answer',
-      body: 'New answer for question',
-      icon: 'https://nimbl3.com/assets/image/favicon.ico?1399950378',
-      open_url: 'http://localhost:3000/questions'
-    }
-    Webpush.payload_send(
-      endpoint: subscription.endpoint,
-      message: JSON.generate(message),
-      p256dh: subscription.key_p256dh,
-      auth: subscription.key_auth,
-      api_key: API_KEY
-    )
   end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,13 +1,37 @@
 class Answer < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   belongs_to :question
 
   validates :content, :question, presence: true
 
   after_create :send_broadcast
+  after_create :push_notification
 
   private
 
   def send_broadcast
     ActionCable.server.broadcast "answers_#{question.id}", id: id, content: content
+  end
+
+  def push_notification
+    message = {
+      title: 'New answer',
+      body: "New answer for question #{question.content}. CLick to view it.",
+      icon: 'https://nimbl3.com/assets/image/favicon.ico?1399950378',
+      open_url: question_path(question)
+    }
+
+    subscriptions = Subscription.all
+
+    subscriptions.each do |subscription|
+      Webpush.payload_send(
+        endpoint: subscription.endpoint,
+        message: JSON.generate(message),
+        p256dh: subscription.key_p256dh,
+        auth: subscription.key_auth,
+        api_key: ENV.fetch('FIRE_BASE_API_KEY')
+      )
+    end
   end
 end
