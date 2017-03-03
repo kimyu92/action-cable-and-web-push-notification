3fdf57c83d9849275f5d7b05b2ad718cd9bddfcc
An Duong <an@nimbl3.com>
2017-03-03 15:24:42 +0700

    Add sidekiq for background job

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,4 @@
 class Answer < ApplicationRecord
-  include Rails.application.routes.url_helpers
-
   belongs_to :question
 
   validates :content, :question, presence: true
@@ -15,23 +13,6 @@ class Answer < ApplicationRecord
   end
 
   def push_notification
-    message = {
-      title: 'New answer',
-      body: "New answer for question #{question.content}. CLick to view it.",
-      icon: 'https://nimbl3.com/assets/image/favicon.ico?1399950378',
-      open_url: question_path(question)
-    }
-
-    subscriptions = Subscription.all
-
-    subscriptions.each do |subscription|
-      Webpush.payload_send(
-        endpoint: subscription.endpoint,
-        message: JSON.generate(message),
-        p256dh: subscription.key_p256dh,
-        auth: subscription.key_auth,
-        api_key: ENV.fetch('FIRE_BASE_API_KEY')
-      )
-    end
+    PushNotificationJob.perform_later(question)
   end
 end
